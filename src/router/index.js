d3cf00362721a49d99e0e1317baa8d0f45ef14de
fan <fan@163.com>
2019-04-22 15:20:43 +0800

    登陆制作完成

--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,14 @@ import Login from '@/components/login.vue'
 import Home from '@/components/Home.vue'
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
+      component: Login
+    },
+    {
+      path: '/login',
       component: Login
     },
     {
@@ -16,3 +20,12 @@ export default new Router({
     }
   ]
 })
+// 路由守卫
+router.beforeEach((to, from, next) => {
+  var token = window.sessionStorage.getItem('token')
+  if (token === null && to.path !== '/login') {
+    return next('/login')
+  }
+  next()
+})
+export default router
